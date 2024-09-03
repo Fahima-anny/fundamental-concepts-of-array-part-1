ae5921d8973ee4e284dd60013b44c6ff5ad6e271
Fahima-anny <fahima.info03@gmail.com>
2024-09-04 02:47:52 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+// let fruits = ["Mango", "Apple", "Pineapple", "Banana", "Grape"] ;
+// console.log(fruits[3]) ;
+// fruits[2] = "Jambura" ;
+// console.log(fruits) ;
+
+
+
+// let destination = ["Cox's-Bazar", "Kashmir", "Ireland"] ;
+// destination.push("New'zealand") ;
+// destination.push("Darjeeling", "Delhi") ;
+// destination.pop() ;
+// console.log(destination) ;
+
+
+
+
+// let books =["javascript", "story", "rhymes", "theory", "biology"] ;
+// let isPresentOrNot = books.includes("javascript") ;
+// if(isPresentOrNot){
+//     console.log("Yes js is present")
+// }
+// else{
+//     console.log("No its not present") ;
+// }
+
+
+
+
+// let people = "me" ;
+// let friends = ["Tamu", "Kuri", "Tua", "Momo"] ;
+// let dhaka = [] ;
+// let strong = true ;
+
+// console.log(Array.isArray(people))
+// console.log(Array.isArray(friends))
+// console.log(Array.isArray(dhaka))
+// console.log(Array.isArray(strong))
+
+
+
+
+let array1 = ["fahima", "yusuf", "faruk"]
+let array2 = ["shova", "khadiza", "arif"]
+
+let newArray = array1.concat(array2) ;
+console.log(array1)
+console.log(array2)
+console.log(newArray)
+
+
+
+
+
